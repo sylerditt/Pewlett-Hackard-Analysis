a984cf85a25f13e857f3ccb507addbfd00e28b97
Taryn Syler <sylerditt@gmail.com>
2022-04-13 17:50:14 -0700

    deliverable 2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -28,3 +28,21 @@ INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY title
 ORDER BY COUNT(title) DESC;
+
+-- deliverable 2
+SELECT DISTINCT ON(e.emp_no)e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility
+FROM employees AS e
+	INNER JOIN dept_emp AS de
+		ON (e.emp_no = de.emp_no)
+			INNER JOIN titles AS ti
+				ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY emp_no;
